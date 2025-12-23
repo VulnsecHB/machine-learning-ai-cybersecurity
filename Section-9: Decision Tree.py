d9a3df37f81afd41886beb73f1a5a2e4138dcb76
Vulnsec-417 <diachoknazar11@gmail.com>
2025-12-23 02:11:30 +0000

    Delete Section-9: Decision Tree.py

--- Section-9: Decision Tree.py
+++
@@ -1,9 +0,0 @@
-from sklearn.model_selection import train_test_split
-
-X = ["win a free iphone", "meeting at 10am"]
-y = ["spam", "ham"]
-
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.5)
-
-print("Train: ", X_train, y_train)
-print("Test: ", X_test, y_test)
