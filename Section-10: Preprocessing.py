3f89ebc343a0df7a1f6716c313c70b5fb220e0f3
Vulnsec-417 <diachoknazar11@gmail.com>
2025-12-23 02:11:46 +0000

    Delete Section-10: Preprocessing.py

--- Section-10: Preprocessing.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn.cluster import KMeans
-
-hours = np.array([[9],[10],[11],[3],[2],[1],[32]])
-lables = KMeans(n_clusters=3, n_init=10).fit(hours).labels_
-# print(list(zip(hours.ravel(), lables)))
-
-print([(int(h), int(l)) for h, l in zip(hours.ravel(), lables)])
