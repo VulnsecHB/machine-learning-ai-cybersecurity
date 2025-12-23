c1bcfadd71cb7e5d8283d2e0d0db372c35d46ceb
Vulnsec-417 <diachoknazar11@gmail.com>
2025-12-23 02:11:54 +0000

    Delete Section-7: Signature based system.py

--- Section-7: Signature based system.py
+++
@@ -1,8 +0,0 @@
-def rule_spam(text):
-    if "free" in text.lower():
-        return "spam"
-    else:
-        return "ham"
-
-print(rule_spam("Win a free Iphone"))
-print(rule_spam("Meeting at 10am"))
