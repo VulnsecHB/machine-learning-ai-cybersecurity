33b7c5b11639e4e1a2b151e6f409ef33b32a544c
Vulnsec-417 <diachoknazar11@gmail.com>
2025-12-23 02:11:40 +0000

    Delete Section-8: Supervised Learning.py

--- Section-8: Supervised Learning.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import CountVectorizer
-
-docs = ["win a free iphone", "meeting tomorrow"]
-vec = CountVectorizer()
-X = vec.fit_transform(docs)
-
-print(vec.get_feature_names_out())
-print(X.toarray())
